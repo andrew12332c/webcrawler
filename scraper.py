e3f762e4bc15ad455f2f6c6e1bd31eb4f5668cb9
samuelacook-web <sacook1@uci.edu>
2026-02-10 14:59:06 -0800

    Uncomment import statements from PartA

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import re
 from urllib.parse import urlparse, urljoin, urldefrag, parse_qs
 from bs4 import BeautifulSoup
 from collections import Counter, defaultdict
-#from PartA import tokenize, computeWordFrequencies, printFrequencies
+from PartA import tokenize, computeWordFrequencies, printFrequencies
 
 ALLOWED_DOMAINS = ("ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu",)
 
